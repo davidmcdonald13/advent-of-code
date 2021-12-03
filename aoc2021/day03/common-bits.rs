b8b407f6d05bf3426c69aa11c43dba1f83f78e46
David McDonald <davidmcdonald13@gmail.com>
2021-12-02 23:37:43 -0800

    Day 3, problem 2

--- aoc2021/day03/common-bits.rs
+++ aoc2021/day03/common-bits.rs
@@ -14,7 +14,7 @@ fn main() {
 
     let mut one_counts: Vec<usize> = vec![0; measure_length];
 
-    for measure in measures {
+    for measure in &measures {
         measure.chars().enumerate().for_each(|(i, c)| {
             if c == '1' {
                 one_counts[i] += 1;
@@ -22,22 +22,56 @@ fn main() {
         });
     }
 
-    let mut gamma_vec: Vec<String> = vec!["0".to_string(); measure_length];
-    let mut epsilon_vec: Vec<String> = vec!["0".to_string(); measure_length];
-    one_counts.iter().enumerate().for_each(|(i, count)| {
-        if count * 2 > total {
-            gamma_vec[i] = "1".to_string();
+    let dominant_chars: Vec<char> = one_counts.into_iter().map(|count| {
+        if (2 * count) >= total {
+            return '1';
         } else {
-            epsilon_vec[i] = "1".to_string();
+            return '0';
         }
-    });
+    }).collect();
+
+    get_power(&dominant_chars);
+    println!();
+    get_life_support(measures);
+}
+
+// this function is overly repetitive, but I'm tired and want to go to bed
+fn get_life_support(measures: Vec<&str>) {
+    let mut oxygen_candidates: Vec<&str> = measures.clone();
+    let mut index: usize = 0;
+    while oxygen_candidates.len() > 1 {
+        let ones: usize = oxygen_candidates.iter().map(|measure| if measure.chars().nth(index).unwrap() == '1' { 1 } else { 0 }).sum();
+        let target_char: char = if (2 * ones) >= oxygen_candidates.len() { '1' } else { '0' };
+        oxygen_candidates = oxygen_candidates.into_iter().filter(|measure| measure.chars().nth(index).unwrap() == target_char).collect();
+        index += 1
+    }
+    let oxygen: usize = usize::from_str_radix(&oxygen_candidates[0], 2).unwrap();
+
+    let mut co2_candidates: Vec<&str> = measures.clone();
+    index = 0;
+    while co2_candidates.len() > 1 {
+        let ones: usize = co2_candidates.iter().map(|measure| if measure.chars().nth(index).unwrap() == '1' { 1 } else { 0 }).sum();
+        let target_char: char = if (2 * ones) < co2_candidates.len() { '1' } else { '0' };
+        co2_candidates = co2_candidates.into_iter().filter(|measure| measure.chars().nth(index).unwrap() == target_char).collect();
+        index += 1
+    }
+    let co2: usize = usize::from_str_radix(&co2_candidates[0], 2).unwrap();
+
+    println!("oxygen: {}", oxygen);
+    println!("co2: {}", co2);
+    println!("life support: {}", oxygen * co2);
+}
+
+fn get_power(dominant_chars: &Vec<char>) {
+    let gamma_vec: Vec<String> = dominant_chars.iter().map(|dom| dom.to_string()).collect();
+    let epsilon_vec: Vec<String> = dominant_chars.iter().map(|dom| if *dom == '1' { '0'.to_string() } else { '1'.to_string() }).collect();
 
     let gamma: usize = vec_to_int(gamma_vec);
     let epsilon: usize = vec_to_int(epsilon_vec);
 
     println!("gamma: {}", gamma);
     println!("epsilon: {}", epsilon);
-    println!("result: {}", gamma * epsilon);
+    println!("power consumption: {}", gamma * epsilon);
 }
 
 fn vec_to_int(v: Vec<String>) -> usize  {
