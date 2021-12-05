b862a0f5b163d49641aecdeb1ddf613be3d7344f
David McDonald <davidmcdonald13@gmail.com>
2021-12-05 14:12:23 -0800

    Day 5, problem 2

--- aoc2021/day05/intersecting-lines.rs
+++ aoc2021/day05/intersecting-lines.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::cmp;
 use std::env;
 use std::fs;
 use std::str;
@@ -8,11 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let filename: &String = &args[1];
+    let is_part_2: bool = true;
 
     let contents: String = fs::read_to_string(filename).expect("failed to read file");
     let lines: str::Lines = contents.lines();
 
-    let mut points: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut points: HashMap<(isize, isize), isize> = HashMap::new();
 
     for line in lines {
         let mut pairs: str::Split<&str> = line.split(" -> ");
@@ -20,24 +20,44 @@ fn main() {
         let mut start: str::Split<&str> = pairs.next().unwrap().split(",");
         let mut finish: str::Split<&str> = pairs.next().unwrap().split(",");
 
-        let start_x: usize = start.next().unwrap().parse::<usize>().unwrap();
-        let start_y: usize = start.next().unwrap().parse::<usize>().unwrap();
+        let start_x: isize = start.next().unwrap().parse::<isize>().unwrap();
+        let start_y: isize = start.next().unwrap().parse::<isize>().unwrap();
 
-        let finish_x: usize = finish.next().unwrap().parse::<usize>().unwrap();
-        let finish_y: usize = finish.next().unwrap().parse::<usize>().unwrap();
+        let finish_x: isize = finish.next().unwrap().parse::<isize>().unwrap();
+        let finish_y: isize = finish.next().unwrap().parse::<isize>().unwrap();
 
-        if start_x == finish_x || start_y == finish_y {
-            for x in cmp::min(start_x, finish_x)..(cmp::max(start_x, finish_x) + 1) {
-                for y in cmp::min(start_y, finish_y)..(cmp::max(start_y, finish_y) + 1) {
-                    let key: (usize, usize) = (x, y);
-                    let before: usize = *points.get(&key).unwrap_or(&0);
-                    points.insert(key, before + 1);
+        let x_dir: isize = get_direction(start_x, finish_x);
+        let y_dir: isize = get_direction(start_y, finish_y);
+
+        if is_part_2 || (start_x == finish_x || start_y == finish_y) {
+            let mut x: isize = start_x;
+            let mut y: isize = start_y;
+            loop {
+                let key: (isize, isize) = (x, y);
+                let before: isize = *points.get(&key).unwrap_or(&0);
+                points.insert(key, before + 1);
+
+                if x == finish_x &&  y == finish_y {
+                    break;
                 }
+
+                x += x_dir;
+                y += y_dir;
             }
         }
     }
 
-    let multi_points: Vec<&usize> = points.values().filter(|count| **count > 1).collect();
+    let multi_points: Vec<&isize> = points.values().filter(|count| **count > 1).collect();
 
     println!("result: {}", multi_points.len());
+}
+
+fn get_direction(start: isize, finish: isize) -> isize {
+    if start == finish {
+        return 0;
+    } else if start < finish {
+        return 1;
+    } else {
+        return -1;
+    }
 }
