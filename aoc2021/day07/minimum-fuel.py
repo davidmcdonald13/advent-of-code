b9f8a98483344c67c4f74f6b31d66cd70425341c
David McDonald <davidmcdonald13@gmail.com>
2021-12-06 21:34:34 -0800

    Day 7, part 2

--- aoc2021/day07/minimum-fuel.py
+++ aoc2021/day07/minimum-fuel.py
@@ -1,12 +1,20 @@
 import sys
 
 filename = sys.argv[1]
+algo = sys.argv[2]
 
 with open(filename, "r") as f:
     positions = [int(x) for x in f.readline().split(",")]
 
 result = list()
 for target in range(min(positions), max(positions) + 1):
-    result.append(sum([abs(target - x) for x in positions]))
+    if algo == "linear":
+        result.append(sum([abs(target - x) for x in positions]))
+    elif algo == "quadratic":
+        aggregator = 0
+        for x in positions:
+            diff = abs(target - x)
+            aggregator += (diff ** 2 + diff) / 2
+        result.append(aggregator)
 
 print(min(result))
