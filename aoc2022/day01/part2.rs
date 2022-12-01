9d33c2015feba15acfcb81cfb0a3ffeadcbbd53a
David McDonald <davidmcdonald13@gmail.com>
2022-11-30 21:46:41 -0800

    2022 day 1

---
+++ aoc2022/day01/part2.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename: &String = &args[1];
+
+    let mut values: Vec<i64> = vec![0];
+
+    let contents: String = fs::read_to_string(filename).expect("failed to read file");
+    let lines: std::str::Lines = contents.lines();
+
+    for line in lines {
+        if line.is_empty() {
+            values.push(0);
+        } else {
+            let index: usize = values.len() - 1;
+            values[index] += line.parse::<i64>().unwrap();
+        }
+    }
+
+    values.sort_unstable();
+
+    let sum: i64 = values[values.len()-3..].iter().sum();
+    println!("result: {}", sum);
+}
