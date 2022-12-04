70677c9142d4bed03d03e16299dd657dbb5ce04e
David McDonald <davidmcdonald13@gmail.com>
2022-12-03 22:04:14 -0800

    2022 day 4

---
+++ aoc2022/day04/part2.rs
@@ -0,0 +1,26 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename: &String = &args[1];
+
+    let contents: String = fs::read_to_string(filename).expect("failed to read file");
+    let lines: std::str::Lines = contents.lines();
+
+    let parsed_values: Vec<Vec<i64>> = lines.map(|line| line.split(&[',', '-']))
+                                            .map(|vals| vals.map(|val| val.parse::<i64>().unwrap()))
+                                            .map(|vals| vals.collect())
+                                            .collect();
+
+    let result = parsed_values.iter().filter(|vals| {
+        let start_a = vals[0];
+        let end_a = vals[1];
+        let start_b = vals[2];
+        let end_b = vals[3];
+
+        return start_a <= end_b && start_b <= end_a;
+    });
+
+    println!("result: {}", result.count());
+}
