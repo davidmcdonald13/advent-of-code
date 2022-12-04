75186c73df21268b5e00cec03c9765be2a1965cc
David McDonald <davidmcdonald13@gmail.com>
2022-12-03 22:04:23 -0800

    Add base.rs

---
+++ aoc2022/base.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename: &String = &args[1];
+
+    let contents: String = fs::read_to_string(filename).expect("failed to read file");
+    let lines: std::str::Lines = contents.lines();
+
+    let result: i64 = 0;
+
+    println!("result: {}", result);
+}
