484e9e1d49f7d029b0ba4c932e6278a5bb92ae1e
David McDonald <davidmcdonald13@gmail.com>
2023-12-10 21:40:34 -0800

    2023 day 11

---
+++ aoc2023/day11/part2.rs
@@ -0,0 +1,82 @@
+use std::cmp;
+use std::env;
+use std::fs;
+
+fn main() {
+    unit_tests();
+
+    let args: Vec<String> = env::args().collect();
+    let filename: &String = &args[1];
+
+    let contents: String = fs::read_to_string(filename).expect("failed to read file");
+    let result = run(contents, 1000000);
+    println!("result: {}", result);
+}
+
+fn run(input: String, expansion_factor: usize) -> usize {
+    let arr: Vec<Vec<char>> = input.lines()
+                                   .map(|line| line.chars().collect())
+                                   .collect();
+
+    let galaxies: Vec<(usize, usize)> = arr.iter()
+                                           .enumerate()
+                                           .flat_map(|(i, row)| row.iter()
+                                                                   .enumerate()
+                                                                   .filter(|(_, val)| **val == '#')
+                                                                   .map(move |(j, _)| (i, j))
+                                           )
+                                           .collect();
+
+    let empty_rows: Vec<usize> = arr.iter()
+                                    .enumerate()
+                                    .filter(|(_, row)| row.iter().all(|c| *c == '.'))
+                                    .map(|(i, _)| i)
+                                    .collect();
+    let empty_cols: Vec<usize> = (0..arr[0].len()).filter(|j| arr.iter()
+                                                                 .all(|row| row[*j] == '.'))
+                                                  .collect();
+
+    let mut result: usize = 0;
+    for i in 0..galaxies.len() {
+        for j in (i+1)..galaxies.len() {
+            let row_iter: std::ops::Range<usize> = cmp::min(galaxies[i].0, galaxies[j].0)..cmp::max(galaxies[i].0, galaxies[j].0);
+            let col_iter: std::ops::Range<usize> = cmp::min(galaxies[i].1, galaxies[j].1)..cmp::max(galaxies[i].1, galaxies[j].1);
+
+            let row_dist: usize = get_size(&row_iter) + empty_rows.iter().filter(|x| row_iter.contains(x)).count() * (expansion_factor - 1);
+            let col_dist: usize = get_size(&col_iter) + empty_cols.iter().filter(|x| col_iter.contains(x)).count() * (expansion_factor - 1);
+            result += row_dist + col_dist;
+        }
+    }
+
+    return result;
+}
+
+fn get_size(range: &std::ops::Range<usize>) -> usize {
+    return range.end - range.start;
+}
+
+fn unit_tests() -> () {
+    assert!(run("...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....".to_string(), 10) == 1030);
+
+    assert!(run("...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....".to_string(), 100) == 8410);
+
+    println!("Unit tests passed");
+}
